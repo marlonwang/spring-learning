062598a23cae6f42126f2a74ab630baa4d9fc4b9
marlonwang <marlonwang@163.com>
2016-12-07 12:13:37 +0800

    config-server/client

---
+++ spring-cloud/config-client/src/main/java/net/logvv/scdemo/config/controller/ClientController.java
@@ -0,0 +1,18 @@
+package net.logvv.scdemo.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class ClientController {
+	@Value("${from}")
+	private String from;
+	
+	@RequestMapping("/from")
+    public String from() {
+        return this.from;
+    }
+}
