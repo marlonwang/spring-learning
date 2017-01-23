b60a61a211d80b53364ca65a3e546b92e29fa01a
marlonwang <marlonwang@163.com>
2017-01-23 15:35:46 +0800

    pom jdbc

--- spring-boot/shiro/src/test/java/net/logvv/shiro/ApplicationTests.java
+++ spring-boot/shiro/src/test/java/net/logvv/shiro/ApplicationTests.java
@@ -1,16 +1,56 @@
 package net.logvv.shiro;
 
+import net.logvv.shiro.controller.HomeController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class ApplicationTests {
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class ApplicationTests
+{
+    @Autowired
+    protected WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup(){
+        mockMvc = MockMvcBuilders.standaloneSetup(new HomeController()).build();
+    }
 
 	@Test
-	public void contextLoads() {
+	public void testHomeController() throws Exception{
+        // hello
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World")));
+
+        RequestBuilder request = null;
+
+        // 获取用户信息
+        mockMvc.perform(MockMvcRequestBuilders.get("/account")
+                .param("firstname","wysiwyg")
+                .param("lastname","qwertyuiop"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("success")));
 	}
+
+
 
 }
